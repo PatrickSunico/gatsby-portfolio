4ac9b06257930bcae577ba7a5b1df60828549fb2
PatrickSunico <apatricksunico@gmail.com>
2021-03-18 00:07:16 +0300

    updated hero and about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,35 +1,61 @@
 import React from "react";
 import ImagePortrait from "../../images/image-portrait.jpg";
+// import PatternBG from "../../images/bg-pattern.png";
+import PatternBGLeft from "../../images/bg-pattern-left.svg";
+import PatternBGRight from "../../images/bg-pattern-right.svg";
 
 const About = () => {
     return (
-        <div className="about">
-            <div className="flex-1 about__left">
-                <div className="about__image-placement"></div>
-            </div>
+        <section className="about-parent">
+            <div className="about-container">
+                {/* Left */}
+                <div
+                    className="flex-1 about-container__left"
+                    style={{ backgroundImage: `url(${ImagePortrait})` }}
+                >
+                    <div className="about-container__image-placement w-full">
+                        <img
+                            src={ImagePortrait}
+                            alt="portrait"
+                            className="w-full"
+                        />
+                    </div>
+                </div>
 
-            <div className="flex-1 about__right">
-                <div className="about__description">
-                    <h2>
-                        Hello, I’m Patrick Frontend Developer and UI Designer
-                    </h2>
-                    <br />
-                    <br />
-                    <br />
-                    <p>
-                        I'm a dreamer and a web aficionado, I like to implement
-                        ideas into an actual product and build up on that idea
-                        to make it more robust and scalable when it comes to
-                        websites,
+                {/* Right */}
+                <div className="flex-1 about-container__right px-4">
+                    <div
+                        className="about-container__bg-pattern-left"
+                        style={{ backgroundImage: `url(${PatternBGLeft})` }}
+                    ></div>
+
+                    <div
+                        className="about-container__bg-pattern-right"
+                        style={{ backgroundImage: `url(${PatternBGRight})` }}
+                    ></div>
+
+                    <div className="about-container__description">
+                        <h2>
+                            Hello, I’m Patrick Frontend Developer and UI
+                            Designer
+                        </h2>
                         <br />
                         <br />
-                        Here you can see a selection of my best web design
-                        wireframes not just websites, but right brain stuffs too
-                        such as logo designs and many more
-                    </p>
+                        <p>
+                            I'm a dreamer and a web aficionado, I like to
+                            implement ideas into an actual product and build up
+                            on that idea to make it more robust and scalable
+                            when it comes to websites,
+                            <br />
+                            <br />
+                            Here you can see a selection of my best web design
+                            wireframes not just websites, but right brain stuffs
+                            too such as logo designs and many more
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,12 +8,14 @@ const Navbar = ({ siteTitle }) => (
             <div className="flex items-center justify-between py-3 navbar__inner-container">
                 {/* Logo */}
                 <div className="flex-shrink-0 flex items-center">
-                    <h1 className="text-3xl navbar__logo">PS</h1>
+                    <h3 className="text-3xl navbar__logo">PS</h3>
+
+                    {/* <h1 className="text-3xl navbar__logo">PS</h1> */}
                 </div>
 
                 {/* Menu */}
                 <div className="items-end">
-                    <div className="flex space-x-10 xs:space-x-10 sm:space-x-10 md:space-x-16 lg:space-x-28 xl:space-x-28">
+                    <div className="flex space-x-10 xs:space-x-16 sm:space-x-16 md:space-x-18 lg:space-x-28">
                         <Link
                             to="/"
                             className="text-white	px-3 py-2 rounded-md text-sm font-medium"
