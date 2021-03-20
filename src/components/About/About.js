6299559a9aab190b8a5e04c1a7dadca9fe7c6b84
PatrickSunico <apatricksunico@gmail.com>
2021-03-20 18:28:22 +0300

    trying to update my font sizes with calc

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,10 +7,24 @@ import PatternBGRight from "../../images/bg-pattern-right.svg";
 const About = () => {
     return (
         <section className="about">
+            <div
+                className="about__bg-pattern-left"
+                style={{ backgroundImage: `url(${PatternBGLeft})` }}
+            ></div>
+
+            <div
+                className="about__bg-pattern-right"
+                style={{ backgroundImage: `url(${PatternBGRight})` }}
+            ></div>
+
             {/* Element */}
             <div className="about__container">
                 {/* Left */}
                 <div className="flex-1 about__left">
+                    <div
+                        className="about__image-placement"
+                        style={{ backgroundImage: `url(${ImagePortrait})` }}
+                    ></div>
                     {/* <div className="about__container__image-placement w-full">
                         <img
                             src={ImagePortrait}
@@ -21,36 +35,32 @@ const About = () => {
                 </div>
 
                 {/* Right */}
-                <div className="flex-1 about__right px-4">
-                    {/* <div
-                        className="about__container__bg-pattern-left"
-                        style={{ backgroundImage: `url(${PatternBGLeft})` }}
-                    ></div>
-
-                    <div
-                        className="about__container__bg-pattern-right"
-                        style={{ backgroundImage: `url(${PatternBGRight})` }}
-                    ></div> */}
-
+                <div className="flex-1 about__right ">
                     <div className="about__description">
-                        <h2>
-                            This is a custom feature of webpack
-                            {/* Hello, I’m Patrick Frontend Developer and UI
-                            Designer */}
-                        </h2>
-                        <br />
-                        <br />
-                        <p>
-                            I'm a dreamer and a web aficionado, I like to
-                            implement ideas into an actual product and build up
-                            on that idea to make it more robust and scalable
-                            when it comes to websites,
+                        <div className="about__text-details">
+                            <h2>
+                                Hello, I’m Patrick Frontend Developer and UI
+                                Designer
+                            </h2>
                             <br />
                             <br />
-                            Here you can see a selection of my best web design
-                            wireframes not just websites, but right brain stuffs
-                            too such as logo designs and many more
-                        </p>
+                            <br />
+                            <p>
+                                I'm a dreamer and a web aficionado, I like to
+                                implement ideas into an actual product and build
+                                up on that idea to make it more robust and
+                                scalable when it comes to websites,
+                                <br />
+                                <br />
+                                <br />
+                                Here you can see a selection of my best web
+                                design wireframes not just websites, but right
+                                brain stuffs too such as logo designs and many
+                                more
+                            </p>
+                        </div>
+
+                        <div className="about__experience"></div>
                     </div>
                 </div>
             </div>
