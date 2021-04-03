4a1191a05acbaa380ef84c48d5e6fc470c298d92
PatrickSunico <apatricksunico@gmail.com>
2021-04-03 18:08:30 +0300

    update

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "gatsby";
+import Intlq from "../../images/image-portrait.jpg";
+
+// import ImagePortrait from "../../images/image-portrait.jpg";
 
 const Portfolio = (props) => {
     return (
         <section className="portfolio l-container">
-            <div className="l-row">
+            <div className="l-column">
                 <div className="portfolio-selector">
                     <h3 className="font-size__lg font-weight__700">
                         Latest Work
@@ -15,30 +19,32 @@ const Portfolio = (props) => {
                         <p>Apps</p>
                     </div>
                 </div>
-                {/* <h1>Portfolio</h1> */}
+
+                {/* list of projects */}
+                <div className="porfolio-list">
+                    <div className="portfolio-item-container">
+                        <div className="portfolio-text-container">
+                            <h3>UI, UX, Web Design</h3>
+                            <h2>INTLQ</h2>
+                            <Link to="/">View Project</Link>
+                            <div className="portfolio-social-icons">
+                                <Link to="/">Behance</Link>
+                                <Link to="/">Website</Link>
+                                <Link to="/">Appstore</Link>
+                                <Link to="/">Android Store</Link>
+                            </div>
+                        </div>
+                        <div className="portfolio-image-container">
+                            <img
+                                src={Intlq}
+                                alt="project"
+                                className="portolio-img offset-image"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
-        // <div className="l-container">
-        //     <div className="l-row">
-        //         <div className="column-12"></div>
-        //         <div className="column-6">6</div>
-
-        //         <div className="column-6">6</div>
-
-        //         {/* <div className="column-12">12</div>
-        //         <div className="column-11">11</div>
-        //         <div className="column-10">10</div>
-        //         <div className="column-9">9</div>
-        //         <div className="column-8">8</div>
-        //         <div className="column-7">7</div>
-        //         <div className="column-6">6</div>
-        //         <div className="column-5">5</div>
-        //         <div className="column-4">4</div>
-        //         <div className="column-3">3</div>
-        //         <div className="column-2">2</div>
-        //         <div className="column-1">1</div> */}
-        //     </div>
-        // </div>
     );
 };
 
